7804e936b1abd0904de34e6af3c3b8aa0bc00c6e
baissou45 <tbaissou@gmail.com>
2024-09-12 20:29:18 +0200

    r

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\Heure;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,14 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void {
+        $faker = Faker::create(); // Créer une instance de Faker
+
+        User::create([
+            'nom_complet' => $faker->firstName() . ' ' . $faker->lastName(),
+            'role' => 'admin',
+            'email' => "a@a.aa",
+            'password' => Hash::make('aaaaaaaa')
+        ]);
 
         $jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
         for ($i=1; $i <= 7; $i++) {
