7132474568bf85d3721cf3823408d20b0b415132
unknown <alexalix58@gmail.com>
2025-01-25 18:28:21 +0100

    Update: configuration files and README.md optimizations

--- config/queue.php
+++ config/queue.php
@@ -41,6 +41,7 @@ return [
             'queue' => env('DB_QUEUE', 'default'),
             'retry_after' => (int) env('DB_QUEUE_RETRY_AFTER', 90),
             'after_commit' => false,
+            'database' => env('DB_CONNECTION', 'pgsql'),
         ],
 
         'beanstalkd' => [
@@ -86,7 +87,7 @@ return [
     */
 
     'batching' => [
-        'database' => env('DB_CONNECTION', 'sqlite'),
+        'database' => env('DB_CONNECTION', 'pgsql'),
         'table' => 'job_batches',
     ],
 
@@ -105,7 +106,7 @@ return [
 
     'failed' => [
         'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
-        'database' => env('DB_CONNECTION', 'sqlite'),
+        'database' => env('DB_CONNECTION', 'pgsql'),
         'table' => 'failed_jobs',
     ],
 
